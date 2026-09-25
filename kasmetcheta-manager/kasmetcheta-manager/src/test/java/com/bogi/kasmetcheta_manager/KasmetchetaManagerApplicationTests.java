cd08934b4c018426742a988d0286d9a41bcfefeb
Bogomil Stoev <bogomil.stoev01@gmail.com>
2026-09-25 22:08:16 +0300

    Initial commit

---
+++ kasmetcheta-manager/kasmetcheta-manager/src/test/java/com/bogi/kasmetcheta_manager/KasmetchetaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogi.kasmetcheta_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasmetchetaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
